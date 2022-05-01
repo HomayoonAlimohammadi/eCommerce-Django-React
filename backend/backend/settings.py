0f7fa4b02617c408e7710d85c4c40ab194a2c0d8
HomayoonAlimohammadi <homayoonalimohammadi@gmail.com>
2022-05-01 11:49:26 +0430

    Add Product model

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -123,7 +123,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = '/static/'
+
+MEDIA_ROOT = '/media/uploads/'
+
+MEDIA_URL = '/media/uploads/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,35 @@
+from unicodedata import decimal
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator, MaxValueValidator
+# from .utils import getImageUUID
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Product(models.Model):
+
+    name = models.CharField(max_length=128)
+    image = models.ImageField(upload_to='product_images/', 
+                              blank=True, null=True)
+                            
+    user = models.ForeignKey(User, 
+                             related_name='products', 
+                             on_delete=models.CASCADE)
+
+    brand = models.CharField(max_length=128, blank=True, null=True)
+    category = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    rating = models.IntegerField(default=0,
+                                 validators=[MinValueValidator(0),
+                                            MaxValueValidator(5)])
+    
+    numReviews = models.IntegerField(default=0,
+                                     validators=[MinValueValidator(0)])
+
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    countInStock = models.IntegerField(default=0, 
+                                       validators=[MinValueValidator(0)])
+    
+    createdAt = models.DateTimeField(auto_now_add=True)
